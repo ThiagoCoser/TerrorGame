b8c9ca3fc3477b20911a2cea49963f1b01b73bc8
ThiagoCoser <thiago.coser@gmail.com>
2024-08-20 20:56:14 -0300

    Update

--- Assets/_GameRef/Scripts/JumpScare.cs
+++ Assets/_GameRef/Scripts/JumpScare.cs
@@ -14,7 +14,7 @@ public class JumpScare : MonoBehaviour
 
         scareActiveted = true;
         scareImage.SetActive(true);
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(3);
         scareImage.SetActive(false);
         scareActiveted = false;
 
